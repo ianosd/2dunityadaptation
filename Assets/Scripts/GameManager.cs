c6a47a56bb7e7b3212a3962cad3e61df5d054f19
Ionut Pop <pop.ianosd@gmail.com>
2019-09-27 01:48:38 +0300

    Added menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager Instance {get; private set;}
+
+    public MenuController menuController;
+
+    void Awake()
+    {
+        if (Instance != null){
+            Debug.LogError("GameManager instance is being overriden");
+        }
+        Instance = this;
+    }
+
+    void Start()
+    {
+        PauseGame();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0;
+        SetMenuVisible(true);
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1;
+        SetMenuVisible(false);
+    }
+
+    private void SetMenuVisible(bool visible){
+        menuController.gameObject.SetActive(visible);
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       playButton.onClick.AddListener(GameManager.Instance.ResumeGame);
+       quitButton.onClick.AddListener(Application.Quit);
+    }
+}
